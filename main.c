2f2452c962182a833d4e45a5f882cbfcf2add667
Kachapin <kevinkachapin@gmail.com>
2022-11-17 03:32:34 +0300

    Modification on files

--- main.c
+++ main.c
@@ -1,19 +1,44 @@
-fbcmfnb
 #include "shell.h"
 
 /**
- * bfree - frees a pointer and NULLs the address
- * @ptr: address of the pointer to free
+ * main - entry point
+ * @ac: arg count
+ * @av: arg vector
  *
- * Return: 1 if freed, otherwise 0.
+ * Return: 0 on success, 1 on error
  */
-int bfree(void **ptr)
+int main(int ac, char **av)
 {
-	if (ptr && *ptr)
+	info_t info[] = { INFO_INIT };
+	int fd = 2;
+
+	asm ("mov %1, %0\n\t"
+		"add $3, %0"
+		: "=r" (fd)
+		: "r" (fd));
+
+	if (ac == 2)
 	{
-		free(*ptr);
-		*ptr = NULL;
-		return (1);
+		fd = open(av[1], O_RDONLY);
+		if (fd == -1)
+		{
+			if (errno == EACCES)
+				exit(126);
+			if (errno == ENOENT)
+			{
+				_eputs(av[0]);
+				_eputs(": 0: Can't open ");
+				_eputs(av[1]);
+				_eputchar('\n');
+				_eputchar(BUF_FLUSH);
+				exit(127);
+			}
+			return (EXIT_FAILURE);
+		}
+		info->readfd = fd;
 	}
-	return (0);
+	populate_env_list(info);
+	read_history(info);
+	hsh(info, av);
+	return (EXIT_SUCCESS);
 }
